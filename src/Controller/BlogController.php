34bf45e2fe55204043d36d2391bd9e5623587a50
albertaljeanpierre <albertaljeanpierre@gmail.com>
2020-05-10 22:55:27 +0200

    creation de deux routes

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,12 +8,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/", name="app_default_index" , methods={"GET"})
      */
     public function index()
-    {
-        return $this->render('blog/index.html.twig', [
+    {   // La fonction render prend en deuxième paramètre un tableau nominatif dont la clé coresponds
+        //  à la variable twig et suivis par sa valeur.
+        return $this->render('base.html.twig', [
             'controller_name' => 'BlogController',
         ]);
+    }
+
+
+    /**
+     * @Route("/blog", name="app_default_blog", methods={"GET"})
+     */
+    // Je ne peut pas indiquer   , methods={GET}  dans la route => erreur 500
+    //[Semantical Error] Couldn't find constant GET, method App\Controller\BlogController::blog() in
+    // F:\Projets-PHP\sf-blog\config/routes\../../src/Controller/ (which is being imported from
+    // "F:\Projets-PHP\sf-blog\config/routes/annotations.yaml").
+    // Make sure annotations are installed and enabled.
+    public function blog()
+    {
+        return $this->render('blog/index.html.twig');
     }
 }
