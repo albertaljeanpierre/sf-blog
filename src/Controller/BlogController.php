89efeba887cf07d461e777d3626bf93518019414
albertaljeanpierre <albertaljeanpierre@gmail.com>
2020-05-11 01:44:21 +0200

    Début de l'intégration du template

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,12 +9,17 @@ class BlogController extends AbstractController
 {
     /**
      * @Route("/", name="app_default_index" , methods={"GET"})
+     * Affiche la liste des articles
      */
     public function index()
     {   // La fonction render prend en deuxième paramètre un tableau nominatif dont la clé coresponds
         //  à la variable twig et suivis par sa valeur.
-        return $this->render('base.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('blog/index.html.twig', [
+            'id'        => '1',
+            'title'     => 'Un titre de blog',
+            'content'   => 'Contenu de l article',
+            'image'     => 'https://picsum.photos/200/300',
+            'author'    => 'Jean-Pierre'
         ]);
     }
 
@@ -23,7 +28,9 @@ class BlogController extends AbstractController
      * @Route("/blog", name="app_default_blog", methods={"GET"})
      */
     public function blog()
-    {
+    {/** TODO changer la vue pour une vue détaillée de chaque article
+     *  attention à passer le titre de l'article dans l'url
+     */
         return $this->render('blog/index.html.twig', [
             'id'        => '1',
             'title'     => 'Un titre de blog',
